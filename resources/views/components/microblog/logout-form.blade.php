4d0bb2ea0eed5bb02b4976c3babfbb605042a463
Mahammad Baghirzada <mahammadbaghirzada@gmail.com>
2024-03-23 18:57:01 +0400

    Control Visibility of Like/Dislike Icons and FIX(logout-form)

--- resources/views/components/microblog/logout-form.blade.php
+++ resources/views/components/microblog/logout-form.blade.php
@@ -1,4 +1,5 @@
 <form method="POST" action="{{ route('logout') }}">
+    @csrf
     <button type="submit" class="tracking-widest hover:text-stone-500">
         {{ __('Logout') }}
     </button>
--- resources/views/livewire/like.blade.php
+++ resources/views/livewire/like.blade.php
@@ -1,29 +1,33 @@
 <div class="flex">
     @if (Auth::check())
-        <a wire:key="isliked-{{$post->id}}" wire:click="undoLike" title="undo like" class="cursor-pointer">
-            <x-microblog.images.like-icon class="fill-green-500 hover:stroke-cyan-700" />
+        @if ($isLiked = $post->isLiked())
+            <a wire:key="isliked-{{$post->id}}" wire:click="undoLike" title="undo like" class="cursor-pointer">
+                <x-microblog.images.like-icon class="fill-green-500 hover:stroke-cyan-700" />
 
-        </a>
-
-        <a wire:key="isdisliked-{{$post->id}}" title="you disliked this" class="post">
-            <x-microblog.images.like-icon class="fill-green-300" />
-        </a>
-
-        <a wire:key="is1liked-{{$post->id}}" wire:click="like" title="like" class="cursor-pointer">
-            <x-microblog.images.like-icon class="fill-green-300 hover:stroke-cyan-700" />
-        </a>
-
+            </a>
+        @elseif($isDisliked = $post->isDisliked())
+            <a wire:key="isdisliked-{{$post->id}}" title="you disliked this" class="post">
+                <x-microblog.images.like-icon class="fill-green-300" />
+            </a>
+        @else
+            <a wire:key="is1liked-{{$post->id}}" wire:click="like" title="like" class="cursor-pointer">
+                <x-microblog.images.like-icon class="fill-green-300 hover:stroke-cyan-700" />
+            </a>
+        @endif
         ({{ $likes }})
-        <a wire:key="i2sliked-{{$post->id}}" wire:click="undoDislike" title="undo dislike" class="ml-2 cursor-pointer">
-            <x-microblog.images.unlike-icon class="fill-red-500 hover:stroke-cyan-700" />
-        </a>
-
-        <a wire:key="i3sliked-{{$post->id}}" title="you like this post" class="ml-2">
-            <x-microblog.images.unlike-icon class="fill-red-300" />
-        </a>
-        <a wire:key="i4sliked-{{$post->id}}" wire:click="dislike" title="dislike" class="ml-2 cursor-pointer">
-            <x-microblog.images.unlike-icon class="fill-red-300 hover:stroke-cyan-700" />
-        </a>
+        @if ($isDisliked ?? false)
+            <a wire:key="i2sliked-{{$post->id}}" wire:click="undoDislike" title="undo dislike" class="ml-2 cursor-pointer">
+                <x-microblog.images.unlike-icon class="fill-red-500 hover:stroke-cyan-700" />
+            </a>
+        @elseif($isLiked ?? false)
+            <a wire:key="i3sliked-{{$post->id}}" title="you like this post" class="ml-2">
+                <x-microblog.images.unlike-icon class="fill-red-300" />
+            </a>
+        @else
+            <a wire:key="i4sliked-{{$post->id}}" wire:click="dislike" title="dislike" class="ml-2 cursor-pointer">
+                <x-microblog.images.unlike-icon class="fill-red-300 hover:stroke-cyan-700" />
+            </a>
+        @endif
         ({{ $dislikes }})
     @else
         <a href="#" title="login to like" class="pointer-events-none">
