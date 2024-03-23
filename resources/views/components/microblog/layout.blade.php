38b67782c1b2dc11d2d3766365e10f55c1bfbd4b
Mahammad Baghirzada <mahammadbaghirzada@gmail.com>
2024-03-23 18:27:24 +0400

    Search Component

--- resources/views/components/microblog/layout.blade.php
+++ resources/views/components/microblog/layout.blade.php
@@ -31,24 +31,7 @@
                 <div class="text-3xl hidden md:block text-gray-600 font-medium ml-2 tracking-tight">
                     <a href="{{ url('/') }}">LaravelMicroBlog</a>
                 </div>
-                <div class="ml-4">
-                    <label>
-                        <input
-                            class="placeholder:italic placeholder:text-slate-400 bg-white w-full border-slate-300 rounded-md py-2 pl-9 pr-3 sm:text-sm"
-                            placeholder="Search for posts ..." type="text" name="search"/>
-                    </label>
-                    <ul class="bg-white border border-gray-100 mt-2 absolute">
-                        <li
-                            class="pl-8 pr-2 py-1 border-b-2 border-gray-100 relative hover:bg-yellow-50 hover:text-gray-900">
-                            <a href="{{ route('posts.show', 1) }}">Post title</a>
-                        </li>
-                        <li
-                            class="pl-8 pr-2 py-1 border-b-2 border-gray-100 relative hover:bg-yellow-50 hover:text-gray-900">
-                            <a href="{{ route('posts.show', 1) }}">Post title</a>
-                        </li>
-
-                    </ul>
-                </div>
+                <livewire:search />
             </div>
             {{-- links --}}
             <div class="text-lg hidden md:flex space-x-6">
---
+++ resources/views/livewire/search.blade.php
@@ -0,0 +1,16 @@
+<div class="ml-4">
+    <label>
+        <input
+            class="placeholder:italic placeholder:text-slate-400 bg-white w-full border-slate-300 rounded-md py-2 pl-9 pr-3 sm:text-sm"
+            placeholder="Search for posts ..." type="text" name="search" />
+    </label>
+    <ul class="bg-white border border-gray-100 mt-2 absolute">
+        <li class="pl-8 pr-2 py-1 border-b-2 border-gray-100 relative hover:bg-yellow-50 hover:text-gray-900">
+            <a href="{{ route('posts.show', 1) }}">Post title</a>
+        </li>
+        <li class="pl-8 pr-2 py-1 border-b-2 border-gray-100 relative hover:bg-yellow-50 hover:text-gray-900">
+            <a href="{{ route('posts.show', 1) }}">Post title</a>
+        </li>
+
+    </ul>
+</div>
