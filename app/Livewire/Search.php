f66cf9739aa18ffe45a883fb849a2d14043beff1
Mahammad Baghirzada <mahammadbaghirzada@gmail.com>
2024-03-25 21:13:40 +0400

    Database Query for Searching Posts

--- app/Livewire/Search.php
+++ app/Livewire/Search.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Post;
 use Livewire\Component;
 
 class Search extends Component
@@ -11,7 +12,7 @@ class Search extends Component
     public function render()
     {
         return view('livewire.search', [
-            'posts' => $this->search ? [['title' => $this->search]] : [],
+            'posts' => $this->search ? Post::query()->whereFullText('title', $this->search)->orWhereFullText('content', $this->search)->get() : [],
         ]);
     }
 }
