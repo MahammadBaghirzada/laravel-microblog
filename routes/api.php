8cec79d4f7eaa8c45b760c2c78287f2c6fdf7321
Mahammad Baghirzada <mahammadbaghirzada@gmail.com>
2024-03-30 18:26:40 +0400

    API: Create New Blog Post Through

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/posts/create', [PostController::class, 'store'])->middleware('auth:sanctum');
